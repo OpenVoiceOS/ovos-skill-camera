0cf49273abefaaa6121363f9de31aa8c72bd7fad
JarbasAl <33701864+JarbasAl@users.noreply.github.com>
2026-07-12 20:38:49 +0000

    Increment Version to 1.0.10a1

--- version.py
+++ version.py
@@ -1,8 +1,8 @@
 # START_VERSION_BLOCK
 VERSION_MAJOR = 1
 VERSION_MINOR = 0
-VERSION_BUILD = 9
-VERSION_ALPHA = 2
+VERSION_BUILD = 10
+VERSION_ALPHA = 1
 # END_VERSION_BLOCK
 
 # derived for setuptools dynamic version (do not edit the block above)
